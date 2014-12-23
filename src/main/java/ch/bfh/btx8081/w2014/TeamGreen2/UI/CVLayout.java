37b8d79547b52d4b6761d9724acd0bfa5bd21cc2
drewj1 <drewj1@bfh.ch>
2014-12-23 15:12:30 +0100

    Added Table to the CV to add information of the Patient Sorter to Table was created added Dummy Data of Jane Smith Edited some layout, new label of title, arrangement of components was adjusted for better and simple view

--- src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/CVLayout.java
+++ src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/CVLayout.java
@@ -6,7 +6,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -27,6 +30,7 @@ import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.Table.HeaderClickEvent;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.VerticalLayout;
 
@@ -39,38 +43,43 @@ public class CVLayout extends BorderPanel {
 
 	private static final long serialVersionUID = 1L;
 	private Label title;
+	private Label addevent;
 	private PopupDateField datefield;
 	private TextArea textinput;
 	private Panel panel;
 	private Label space;
 	private Label space1;
 	private Label space2;
+	private Label space3;
 	private Button savebutton;
 	private Button exitbutton;
-
+	
 	public CVLayout() {
 
 		this.title = new Label("Curriculum Vitae");
 		this.title.setWidth(null);
 		this.space = new Label("");
 		this.space1 = new Label("");
+		this.space2 = new Label("");
+		this.space3 = new Label("");
 		this.panel = new Panel();
 		this.panel.setStyleName("borderless");
 		this.panel.setSizeFull();
+		this.addevent = new Label("Add an event to the CV");
 
 		// Datefield
 		this.datefield = new PopupDateField();
 		datefield.setValue(new Date());
-		datefield.setWidth("450px");
+		datefield.setSizeFull();
 		datefield.setImmediate(true);
 		datefield.setTimeZone(TimeZone.getTimeZone("UTC"));
 		datefield.setLocale(Locale.GERMANY);
 
 		// RichTextArea
 		this.textinput = new TextArea();
-		textinput.setValue("Type here the life event of the Patient");
-		textinput.setWidth("450px");
-		textinput.setRows(4);
+		textinput.setValue("Type here the life event of the Patient you want to add.");
+		textinput.setSizeFull();
+		textinput.setRows(2);
 		textinput.setImmediate(true);
 
 		VerticalLayout vertlayout = new VerticalLayout();
@@ -79,12 +88,16 @@ public class CVLayout extends BorderPanel {
 		vertlayout.setComponentAlignment(this.title, Alignment.TOP_CENTER);
 		vertlayout.addComponent(this.space);
 		vertlayout.setComponentAlignment(this.space, Alignment.TOP_CENTER);
+		vertlayout.addComponent(this.addevent);
 		vertlayout.addComponent(this.datefield);
 		vertlayout.setComponentAlignment(this.datefield, Alignment.TOP_CENTER);
 		vertlayout.addComponent(this.textinput);
 		vertlayout.setComponentAlignment(this.textinput, Alignment.TOP_CENTER);
 		vertlayout.addComponent(this.space1);
 		vertlayout.addComponent(createSaveButton());
+		vertlayout.addComponent(this.space2);
+		vertlayout.addComponent(CVTable());
+		vertlayout.addComponent(this.space3);
 		vertlayout.addComponent(createHomeViewButton());
 		
 
@@ -177,6 +190,35 @@ public class CVLayout extends BorderPanel {
 
 		return savebutton;
 
+	}
+	
+	public Table CVTable() {
+		Table cvtable = new Table("CV of the Patient - Jane Smith - PID 123-456-789");
+
+		// Define two columns for the built-in container
+		cvtable.addContainerProperty("Date", String.class, null);
+		cvtable.addContainerProperty("Event",  String.class, null);
+
+		// Add a few other rows using shorthand addItem()
+		cvtable.addItem(new Object[]{"1991", "Death of Mother"}, 1);
+		cvtable.addItem(new Object[]{"1995", "Brother jumped from Bridge, survived"}, 2);
+		cvtable.addItem(new Object[]{"2000", "Married John Smith, police sergeant"}, 3);
+		cvtable.addItem(new Object[]{"2012", "Death of husband, John Smith, local shooting"}, 4);
+		cvtable.sort();
+		
+		// Handle the header clicks
+		cvtable.addHeaderClickListener(new Table.HeaderClickListener() {
+		    public void headerClick(HeaderClickEvent event) {
+		    	cvtable.setSortAscending(true);
+		    }
+		});
+		
+		// Show exactly the currently contained rows (items)
+		cvtable.setPageLength(cvtable.size());
+		cvtable.setSizeFull();
+		
+		return cvtable;
+		
 	}
 	
 	
