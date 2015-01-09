ea2b6702e9f3083408070c77b8731aa19783d3ee
drewj1 <drewj1@bfh.ch>
2015-01-09 16:01:58 +0100

    Documentation added to the class

--- src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/TimelineView.java
+++ src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/TimelineView.java
@@ -1,10 +1,17 @@
 package ch.bfh.btx8081.w2014.TeamGreen2.UI;
 
-
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.VerticalLayout;
+
+/**
+ * The following class provides the methods that the Layout is added to the View
+ * of the application and can be switched to another View.
+ * 
+ * @author JD
+ *
+ */
 
 public class TimelineView extends VerticalLayout implements View {
 	private static final long serialVersionUID = 1L;
@@ -13,8 +20,6 @@ public class TimelineView extends VerticalLayout implements View {
 		setMargin(true);
 		TimelineLayout TimelinePanel = new TimelineLayout();
 		addComponent(TimelinePanel); 
-		
-		// create the "login" panel and add it to the login view.
 		this.setComponentAlignment(TimelinePanel, Alignment.MIDDLE_CENTER);
 	}
 
