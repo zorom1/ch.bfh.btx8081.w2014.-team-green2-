d3c934e95db28c41e68fe86747e281549bc59b5c
drewj1 <drewj1@bfh.ch>
2015-01-09 14:50:15 +0100

    Documentation was added to Class, Class was cleaned up where necessary

--- src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/HomeView/HomeLayout.java
+++ src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/HomeView/HomeLayout.java
@@ -1,8 +1,6 @@
 package ch.bfh.btx8081.w2014.TeamGreen2.HomeView;
 
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 import ch.bfh.btx8081.w2014.TeamGreen2.MHCPMS.MyVaadinUI;
 import ch.bfh.btx8081.w2014.TeamGreen2.UI.AddNewPatientView;
 import ch.bfh.btx8081.w2014.TeamGreen2.UI.CVView;
@@ -18,8 +16,17 @@ import com.vaadin.ui.Panel;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
 
+/**
+ * The following Class provides the Layout of the HomeView. It shows a Table
+ * with all Patients where their meta-data (name, first name, birthdate, patient
+ * ID, etc.) can be viewed
+ * 
+ * @author JD
+ *
+ */
+
 public class HomeLayout extends BorderPanel {
-	
+
 	private static final long serialVersionUID = 1L;
 	private Label title;
 	private Panel panel;
@@ -28,175 +35,183 @@ public class HomeLayout extends BorderPanel {
 	private Button cvbutton;
 	private Button timelinebutton;
 	private Button casebutton;
-	private Button addnewpatientbutton; 
-public static String tableFlag ="0";
-public HomeLayout() {
-	
-	this.title = new Label("Home");
-	this.title.setWidth(null);
-	this.space = new Label("");
-	this.space1 = new Label("");
-	this.panel = new Panel();
-	this.panel.setStyleName("borderless");
-	this.panel.setSizeFull();
+	private Button addnewpatientbutton;
+	public static String tableFlag = "0";
 
-	VerticalLayout vertlayout = new VerticalLayout();
-	FormLayout forlayout = new FormLayout();
-	vertlayout.addComponent(this.title);
-	vertlayout.setComponentAlignment(this.title, Alignment.TOP_CENTER);
-	vertlayout.addComponent(this.space);
-	vertlayout.setComponentAlignment(this.space, Alignment.TOP_CENTER);
-	
-	vertlayout.addComponent(createTable());
-	
-	vertlayout.addComponent(createCVViewButton());
-	vertlayout.addComponent(createTimelineViewButton());
-	vertlayout.addComponent(createCaseButton());
-	vertlayout.addComponent(createNewPatientButton());
+	public HomeLayout() {
 
-	forlayout.addComponent(vertlayout);
-	forlayout.setSizeFull();
-	this.panel.setContent(forlayout);
-	setContent(panel);
-}
-	
-public Table createTable() {
-	Table table = new Table("");
+		this.title = new Label("Home");
+		this.title.setWidth(null);
+		this.space = new Label("");
+		this.space1 = new Label("");
+		this.panel = new Panel();
+		this.panel.setStyleName("borderless");
+		this.panel.setSizeFull();
 
-	// Define two columns for the built-in container
-	table.addContainerProperty("Name", String.class, null);
-	table.addContainerProperty("LastName",  String.class, null);
-	table.addContainerProperty("Gender", String.class, null);
-	table.addContainerProperty("PID", String.class, null);
-	table.addContainerProperty("Birthdate", String.class, null);
+		VerticalLayout vertlayout = new VerticalLayout();
+		FormLayout forlayout = new FormLayout();
+		vertlayout.addComponent(this.title);
+		vertlayout.setComponentAlignment(this.title, Alignment.TOP_CENTER);
+		vertlayout.addComponent(this.space);
+		vertlayout.setComponentAlignment(this.space, Alignment.TOP_CENTER);
 
-	// Add a row the hard way
-	//Object newItemId = table.addItem();
-	//Item row1 = table.getItem(newItemId);
+		vertlayout.addComponent(createTable());
 
-	// Add a few other rows using shorthand addItem()
-	//table.addItem(new Object[]{"12345678","Smith", "Jane", "female", "12.12.2014"}, 1);
-	
-	// Show exactly the currently contained rows (items)
-	
-	/*fill the Table on the HomeView with actual patients data
+		vertlayout.addComponent(createCVViewButton());
+		vertlayout.addComponent(createTimelineViewButton());
+		vertlayout.addComponent(createCaseButton());
+		vertlayout.addComponent(createNewPatientButton());
+
+		forlayout.addComponent(vertlayout);
+		forlayout.setSizeFull();
+		this.panel.setContent(forlayout);
+		setContent(panel);
+	}
+
+	/**
+	 * This method provides the Table where the patient data is displayed.
 	 * 
+	 * @return Table
 	 */
-	String [] temp_elem= new String[5];
-	//if(tableFlag=="0"){
-	try {
-		
-		TableElements telements= new TableElements();
-		telements.countElements();
-		
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	//}//for if
-	if(tableFlag=="3"){
-		
-	}
-	int ArraySize= TableElements.myElements.size();
-	int i=0;
-	/*variable k counts the lines in the table
-	 	 */
-	int k=1;
-	/*array from 5 elements is one line of the table
-	 	 */
-	
-	  while(i<ArraySize-1){
-		  for (int j=0;j<5  ;j++){
-			temp_elem[j] = TableElements.myElements.get(i); 
-		
-			i++;
-		  }
-		  int j=0;
-/*filling Table with data from patients xml file according to toString
-  */
 
-table.addItem(new Object[]{temp_elem[j],temp_elem[j+1],temp_elem[j+2],temp_elem[j+3],temp_elem[j+4]},k);
-		  k++;
-	  }		
-	//}
-	 
-	table.setPageLength(table.size());
-	//table.setPageLength(3);
-	table.setSizeFull();
-	
-	return table;
-	
-	
-}
+	public Table createTable() {
+		Table table = new Table("");
 
-public Button createCVViewButton() {
-	cvbutton = new Button("CV");
-	cvbutton.addClickListener(new Button.ClickListener() {
-		private static final long serialVersionUID = 1L;
+		// Define the columns for the built-in container
+		table.addContainerProperty("Name", String.class, null);
+		table.addContainerProperty("LastName", String.class, null);
+		table.addContainerProperty("Gender", String.class, null);
+		table.addContainerProperty("PID", String.class, null);
+		table.addContainerProperty("Birthdate", String.class, null);
 
-		public void buttonClick(ClickEvent event) {
-			MyVaadinUI.setCVView(new CVView());
+		// Fill the Table on the HomeView with actual patients data
+		String[] temp_elem = new String[5];
+		// if(tableFlag=="0"){
+		try {
+
+			TableElements telements = new TableElements();
+			telements.countElements();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	});
-	return cvbutton;
-}
+		// }//for if
+		if (tableFlag == "3") {
 
-public Button createTimelineViewButton() {
-	timelinebutton = new Button("Timeline");
-	timelinebutton.addClickListener(new Button.ClickListener() {
-		private static final long serialVersionUID = 1L;
-
-		public void buttonClick(ClickEvent event) {
-			MyVaadinUI.setTimelineView(new TimelineView());
 		}
-	});
-	return timelinebutton;
+		int ArraySize = TableElements.myElements.size();
+		int i = 0;
+		/*
+		 * variable k counts the lines in the table
+		 */
+		int k = 1;
+		/*
+		 * array from 5 elements is one line of the table
+		 */
+
+		while (i < ArraySize - 1) {
+			for (int j = 0; j < 5; j++) {
+				temp_elem[j] = TableElements.myElements.get(i);
+
+				i++;
+			}
+			int j = 0;
+
+			// Filling Table with data from patients xml file according to
+			// toString
+			table.addItem(new Object[] { temp_elem[j], temp_elem[j + 1],
+					temp_elem[j + 2], temp_elem[j + 3], temp_elem[j + 4] }, k);
+			k++;
+		}
+		// }
+
+		table.setPageLength(table.size());
+		// table.setPageLength(3);
+		table.setSizeFull();
+
+		return table;
+
+	}
+
+	/**
+	 * This method creates a button where the user can navigate to the Patient
+	 * CV.
+	 * 
+	 * @return CVButton
+	 */
+
+	public Button createCVViewButton() {
+		cvbutton = new Button("CV");
+		cvbutton.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void buttonClick(ClickEvent event) {
+				MyVaadinUI.setCVView(new CVView());
+			}
+		});
+		return cvbutton;
+	}
+
+	/**
+	 * This method creates a button where the user can navigate to the Patient
+	 * Timeline.
+	 * 
+	 * @return TimelineButton
+	 */
+
+	public Button createTimelineViewButton() {
+		timelinebutton = new Button("Timeline");
+		timelinebutton.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void buttonClick(ClickEvent event) {
+				MyVaadinUI.setTimelineView(new TimelineView());
+			}
+		});
+		return timelinebutton;
+	}
+
+	/**
+	 * This method creates a button where the user can navigate to the Patient
+	 * Case.
+	 * 
+	 * @return CaseButton
+	 */
+
+	public Button createCaseButton() {
+		casebutton = new Button("Case");
+		casebutton.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void buttonClick(ClickEvent event) {
+				// MyVaadinUI.setTimelineView(new TimelineView());
+				MyVaadinUI.setCaseView(new CaseView());
+			}
+
+		});
+		return casebutton;
+	}
+
+	/**
+	 * This method creates a button where the user can navigate to the view to
+	 * add a new patient to the database.
+	 * 
+	 * @return AddNewPatientButton
+	 */
+
+	public Button createNewPatientButton() {
+		addnewpatientbutton = new Button("Add new Patient");
+		addnewpatientbutton.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void buttonClick(ClickEvent event) {
+				// MyVaadinUI.setTimelineView(new TimelineView());
+				MyVaadinUI.setAddNewPatientView(new AddNewPatientView());
+			}
+
+		});
+		return addnewpatientbutton;
+	}
+
 }
-public Button createCaseButton(){
-	casebutton= new Button("Case");
-	casebutton.addClickListener(new Button.ClickListener() {
-		private static final long serialVersionUID = 1L;
-
-		public void buttonClick(ClickEvent event) {
-		//	MyVaadinUI.setTimelineView(new TimelineView());
-			MyVaadinUI.setCaseView(new CaseView());	
-		}	
-		
-	});
-	return casebutton;
-}
-
-public Button createNewPatientButton(){
-	addnewpatientbutton = new Button("Add new Patient");
-	addnewpatientbutton.addClickListener(new Button.ClickListener() {
-		private static final long serialVersionUID = 1L;
-
-		public void buttonClick(ClickEvent event) {
-		//	MyVaadinUI.setTimelineView(new TimelineView());
-			MyVaadinUI.setAddNewPatientView(new AddNewPatientView());	
-		}	
-		
-	});
-	return addnewpatientbutton;
-}
-
-
-//	this.label = new Label("Patients");
-//	this.label.setWidth(null);
-//
-//	this.panel = new Panel();
-//	this.panel.addStyleName(getPrimaryStyleName());
-//	this.panel.setSizeFull();
-//	
-//	VerticalLayout vertlayout1 = new VerticalLayout();
-//	FormLayout forlayout1 = new FormLayout();
-//	vertlayout1.addComponent(this.label);
-//	vertlayout1.setComponentAlignment(this.label, Alignment.TOP_CENTER);
-//	
-//	forlayout1.addComponent(vertlayout1);
-//	forlayout1.setSizeFull();
-//	this.panel.setContent(forlayout1);
-//	setContent(panel);
-	
-}
-
