4a09e5bfae4b0c860de9420ca1325b7ac0c19d18
drewj1 <drewj1@bfh.ch>
2015-01-09 15:39:11 +0100

    Documentation added

--- src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/CVView.java
+++ src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/CVView.java
@@ -1,10 +1,17 @@
 package ch.bfh.btx8081.w2014.TeamGreen2.UI;
 
-
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.VerticalLayout;
+
+/**
+ * The following class provides the methods that the Layout is added to the View
+ * of the application and can be switched to another View.
+ * 
+ * @author JD
+ *
+ */
 
 public class CVView extends VerticalLayout implements View {
 	private static final long serialVersionUID = 1L;
@@ -12,9 +19,9 @@ public class CVView extends VerticalLayout implements View {
 	public CVView() {
 		setMargin(true);
 		CVLayout CVPanel = new CVLayout();
-		addComponent(CVPanel); 
-		
-		// create the "login" panel and add it to the login view.
+		addComponent(CVPanel);
+
+		// Create the "login" panel and adds it to the login view.
 		this.setComponentAlignment(CVPanel, Alignment.MIDDLE_CENTER);
 	}
 
