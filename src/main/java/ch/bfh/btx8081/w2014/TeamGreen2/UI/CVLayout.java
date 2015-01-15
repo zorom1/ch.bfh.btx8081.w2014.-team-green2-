68498d56cc74c13098ebd8246da07fa3226f550c
drewj1 <drewj1@bfh.ch>
2015-01-15 13:09:06 +0100

    new

--- src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/CVLayout.java
+++ src/main/java/ch/bfh/btx8081/w2014/TeamGreen2/UI/CVLayout.java
@@ -1,24 +1,19 @@
 package ch.bfh.btx8081.w2014.TeamGreen2.UI;
 
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-
 import ch.bfh.btx8081.w2014.TeamGreen2.HomeView.BorderPanel;
 import ch.bfh.btx8081.w2014.TeamGreen2.HomeView.HomeView;
 import ch.bfh.btx8081.w2014.TeamGreen2.MHCPMS.MyVaadinUI;
 
-import com.vaadin.data.Property;
-import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
-import com.vaadin.ui.PopupDateField;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.Table.HeaderClickEvent;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.VerticalLayout;
 
@@ -35,69 +30,129 @@ public class CVLayout extends BorderPanel {
 	private static final long serialVersionUID = 1L;
 	private Label title;
 	private Label addevent;
-	private PopupDateField datefield;
-	private TextArea textinput;
+	private Label findpid;
+	private TextField date;
+	private TextArea event;
 	private Panel panel;
 	private Label space;
 	private Label space1;
 	private Label space2;
 	private Label space3;
+	private Label space4;
 	private Button savebutton;
 	private Button homebutton;
+	private TextField pid;
+	private Button findPID;
+	private String myPID = null;
+	private Button switchLayout;
 
 	public CVLayout() {
 
 		this.title = new Label("Curriculum Vitae");
 		this.title.setWidth(null);
+		this.findpid = new Label ("Type the PID to view the CV");
 		this.space = new Label("");
 		this.space1 = new Label("");
 		this.space2 = new Label("");
 		this.space3 = new Label("");
+		this.space4 = new Label("");
 		this.panel = new Panel();
 		this.panel.setStyleName("borderless");
 		this.panel.setSizeFull();
 		this.addevent = new Label("Add an event to the CV");
 
 		// Datefield
-		this.datefield = new PopupDateField();
-		datefield.setValue(new Date());
-		datefield.setSizeFull();
-		datefield.setImmediate(true);
-		datefield.setTimeZone(TimeZone.getTimeZone("UTC"));
-		datefield.setLocale(Locale.GERMANY);
-		// Display only year and month in ISO format
-		datefield.setDateFormat("yyyy-MM");
+
+		this.date = new TextField();
+		date.setInputPrompt("1991-05");
 
 		// RichTextArea
-		this.textinput = new TextArea();
-		textinput
-				.setInputPrompt("Type here the life event of the Patient you want to add.");
-		textinput.setSizeFull();
-		textinput.setRows(2);
-		textinput.setImmediate(true);
+		this.event = new TextArea();
+		event.setInputPrompt("Type here the life event of the Patient you want to add.");
+		event.setSizeFull();
+		event.setRows(2);
+		event.setImmediate(true);
 
+		// PID Field
+		this.pid = new TextField();
+		pid.setInputPrompt("p035");
+		pid.setRequired(true);
+		pid.setRequiredError("Type a valid PID");
+
+		// Add Components to the Layout
 		VerticalLayout vertlayout = new VerticalLayout();
 		FormLayout forlayout = new FormLayout();
 		vertlayout.addComponent(this.title);
 		vertlayout.setComponentAlignment(this.title, Alignment.TOP_CENTER);
 		vertlayout.addComponent(this.space);
-		vertlayout.setComponentAlignment(this.space, Alignment.TOP_CENTER);
+		vertlayout.addComponent(this.findpid);
+		vertlayout.addComponent(this.pid);
+		vertlayout.addComponent(findPID());
+		vertlayout.addComponent(this.space4);
 		vertlayout.addComponent(this.addevent);
-		vertlayout.addComponent(this.datefield);
-		vertlayout.setComponentAlignment(this.datefield, Alignment.TOP_CENTER);
-		vertlayout.addComponent(this.textinput);
-		vertlayout.setComponentAlignment(this.textinput, Alignment.TOP_CENTER);
+		vertlayout.addComponent(this.date);
+		vertlayout.addComponent(this.event);
+		vertlayout.setComponentAlignment(this.event, Alignment.TOP_CENTER);
 		vertlayout.addComponent(this.space1);
-		vertlayout.addComponent(createSaveButton());
-		vertlayout.addComponent(this.space2);
-		vertlayout.addComponent(CVTable());
-		vertlayout.addComponent(this.space3);
-		vertlayout.addComponent(createHomeViewButton());
+
+		HorizontalLayout horizlayout = new HorizontalLayout();
+		horizlayout.setSizeFull();
+		horizlayout.addComponent(createSaveButton());
+		horizlayout.addComponent(this.space3);
+		horizlayout.addComponent(switchLayout());
+		horizlayout.addComponent(this.space2);
+		horizlayout.addComponent(createHomeViewButton());
 
 		forlayout.addComponent(vertlayout);
+		forlayout.addComponent(horizlayout);
 		forlayout.setSizeFull();
 		this.panel.setContent(forlayout);
 		setContent(panel);
+
+	}
+
+	/**
+	 * Creates the "Find" Button to find the patients CV in the database.
+	 * 
+	 * @return Button
+	 */
+
+	public Button findPID() {
+		findPID = new Button("Find Patient");
+		findPID.setClickShortcut(KeyCode.ENTER);
+		findPID.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void buttonClick(ClickEvent event) {
+				myPID = pid.getValue();
+
+				CVReadWr cvreadwr = new CVReadWr();
+				cvreadwr.cvread(myPID);
+				Notification.show("Patient found");
+
+			}
+		});
+		return findPID;
+	}
+
+	/**
+	 * Creates the "Switch" Button to exit the View and see the CV of the
+	 * Patient.
+	 * 
+	 * @return Button
+	 */
+
+	public Button switchLayout() {
+		switchLayout = new Button("View CV");
+		switchLayout.addClickListener(new Button.ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void buttonClick(ClickEvent event) {
+				MyVaadinUI.setCVSwitchView(new CVSwitchView());
+
+			}
+		});
+		return switchLayout;
 	}
 
 	/**
@@ -130,76 +185,15 @@ public class CVLayout extends BorderPanel {
 		savebutton.addClickListener(new Button.ClickListener() {
 			private static final long serialVersionUID = 1L;
 
-			public void buttonClick(ClickEvent event) {
-				datefield
-						.addValueChangeListener(new Property.ValueChangeListener() {
+			public void buttonClick(ClickEvent event1) {
+				myPID = pid.getValue();
 
-							@Override
-							public void valueChange(ValueChangeEvent event) {
-								String date = (String) event.getProperty()
-										.getValue();
+				CVReadWr cvreadwr = new CVReadWr();
+				cvreadwr.cvwrite(myPID, date.getValue(), event.getValue());
 
-								// HOW TO ADD DATE IN DB?
-
-							}
-
-						});
-
-				textinput
-						.addValueChangeListener(new Property.ValueChangeListener() {
-							public void valueChange(ValueChangeEvent event) {
-								String value = (String) event.getProperty()
-										.getValue();
-
-								// HOW TO ADD EVENT IN DB?
-
-							}
-
-						});
 			}
-
 		});
 		return savebutton;
-	}
-
-	/**
-	 * This method shows the table which displays the information that was added
-	 * by the doctor or which is in the database.
-	 * 
-	 * @return Table
-	 */
-
-	public Table CVTable() {
-		Table cvtable = new Table(
-				"CV of the Patient - Jane Smith - PID 123-456-789");
-
-		// Define two columns for the built-in container
-		cvtable.addContainerProperty("Date", String.class, null);
-		cvtable.addContainerProperty("Event", String.class, null);
-
-		// Add a few other rows using shorthand addItem()
-		cvtable.addItem(new Object[] { "1991-05", "Death of Mother" }, 1);
-		cvtable.addItem(new Object[] { "1995-03",
-				"Brother jumped from Bridge, survived" }, 2);
-		cvtable.addItem(new Object[] { "2000-12",
-				"Married John Smith, police sergeant" }, 3);
-		cvtable.addItem(new Object[] { "2012-09",
-				"Death of husband, John Smith, local shooting" }, 4);
-		cvtable.sort();
-
-		// Handle the header clicks
-		cvtable.addHeaderClickListener(new Table.HeaderClickListener() {
-			public void headerClick(HeaderClickEvent event) {
-				cvtable.setSortAscending(true);
-			}
-		});
-
-		// Show exactly the currently contained rows (items)
-		cvtable.setPageLength(cvtable.size());
-		cvtable.setSizeFull();
-
-		return cvtable;
-
 	}
 
 }
